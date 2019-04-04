e0ae669bbfd883b17e37c956c5334bcae9db78c8
Filipe <filipeklinger@gmail.com>
2019-04-04 09:54:10 -0300

    Adicionado metodo getJson

--- Aula4/app/src/main/java/com/example/aula4/JsonParser.java
+++ Aula4/app/src/main/java/com/example/aula4/JsonParser.java
@@ -1,8 +1,38 @@
 package com.example.aula4;
 
+import android.content.Context;
+import android.content.res.Resources;
+import android.util.Log;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 public class JsonParser {
+    Context mContext;
+    public JsonParser(Context context){
+        mContext = context;
+    }
+
+    public String getJson(){
+        String json;
+        try {
+            InputStream is = mContext.getAssets().open("climatempo.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            //noinspection ResultOfMethodCallIgnored
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            Log.e("Json Utils","Error Loading FROM Asset");
+            return null;
+        }
+        return json;
+    }
+
 }
--- Aula4/app/src/main/java/com/example/aula4/MainActivity.java
+++ Aula4/app/src/main/java/com/example/aula4/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.aula4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        JsonParser jp = new JsonParser(this);
+        String nossoJson = jp.getJson();
+
+        TextView tv = findViewById(R.id.tv1);
+        tv.setText(nossoJson);
     }
 }
