e9bac0334cd2d7525de5d2147779d831ea42254d
Filipe <filipeklinger@gmail.com>
2019-04-16 09:00:52 -0300

    Adicionado classe Clima

---
+++ Aula4/app/src/main/java/com/example/aula4/Clima.java
@@ -0,0 +1,88 @@
+package com.example.aula4;
+
+import java.util.ArrayList;
+
+public class Clima {
+    private String nome,sigla,country;
+    private ArrayList<Dados> arrayDados;
+
+    //construtor
+    public Clima(){
+        arrayDados = new ArrayList<>();//Inicializa o vetor
+    }
+
+    //setters
+    public void setSigla(String sigla){
+        this.sigla = sigla;
+    }
+    public void setCountry(String country){
+        this.country = country;
+    }
+
+    public void setDados(Dados dados){
+        arrayDados.add(dados);
+    }
+
+    //getters
+    public String getNome() { return nome; }
+
+    public String getSigla() { return sigla; }
+
+    public String getCountry() { return country; }
+
+    public ArrayList<Dados> getArrayDados() { return arrayDados; }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    //objeto dados que representa cada dia sobre o Clima
+    public static class Dados{
+        private String data;
+        private Humidade humidade;
+        private Temperatura temperatura;
+        //construtor da classe
+        public Dados(){
+        }
+
+        //setters
+        public void setData(String data) { this.data = data;}
+        public void setHumidade(int min,int max) { this.humidade = new Humidade(min,max); }
+        public void setTemperatura(int min,int max) { this.temperatura = new Temperatura(min,max); }
+
+        //getters
+        public String getData(){ return this.data; }
+        public Humidade getHumidade(){ return this.humidade;}
+        public Temperatura getTemperatura(){ return this.temperatura;}
+
+
+        //objeto humidade
+        public class Humidade{
+            private int minima,maxima;
+            //construtor da classe aninhada
+            public Humidade(int min,int max){
+                this.minima = min;
+                this.maxima = max;
+            }
+            //getters
+            public int getMinima(){ return this.minima; }
+            public int getMaxima(){ return this.maxima; }
+
+        }
+        //objeto Temp
+        public class Temperatura{
+            private int minima,maxima;
+
+            public Temperatura(int min,int max){
+                this.minima = min;
+                this.maxima = max;
+            }
+
+            //getters
+            public int getMinima(){ return this.minima; }
+            public int getMaxima(){ return this.maxima; }
+
+        }
+    }
+
+}
--- Aula4/app/src/main/java/com/example/aula4/JsonParser.java
+++ Aula4/app/src/main/java/com/example/aula4/JsonParser.java
@@ -17,6 +17,8 @@ public class JsonParser {
         mContext = context;
     }
 
+    //---------------Metodos------------------------
+    //busca o arquivo dentro de "res/raw" contendo o Json e retorna uma string com seu conteudo
     public String getJson(){
         String json;
         try {
@@ -33,6 +35,46 @@ public class JsonParser {
             return null;
         }
         return json;
+    }
+
+    public Clima parse(){
+        Clima clima = new Clima();//instanciando nosso objeto clima
+        String json = getJson();//obtendo nossa string Json
+
+        try {
+            //pegando a string Json transformano em Objeto
+            JSONObject jsonObject = new JSONObject(json);
+
+            //obtendo nome do objeto json
+            String nome = jsonObject.getString("name");
+            //inserindo na classe clima
+            clima.setNome(nome);
+
+            //setando outros atributos com o mesmo processo so que aninhado
+            clima.setSigla(jsonObject.getString("state"));
+            clima.setCountry(jsonObject.getString("country"));
+
+            //fazendo parse dos dados desse clima
+            JSONArray jsonArray = jsonObject.getJSONArray("data");
+            for (int i=0;i< jsonArray.length();i++){
+                Clima.Dados dados = new Clima.Dados();
+                JSONObject dd = jsonArray.getJSONObject(i);
+                dados.setData(dd.getString("date_br"));
+                    //humidade
+                    JSONObject humidade = dd.getJSONObject("humidity");
+                    dados.setHumidade(humidade.getInt("min"),humidade.getInt("max"));
+                    //temperatura
+                    JSONObject temp = dd.getJSONObject("temperature");
+                    dados.setTemperatura(temp.getInt("min"),temp.getInt("max"));
+                //adicionando dados ao clima
+                clima.setDados(dados);
+            }
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return clima;
     }
 
 }
